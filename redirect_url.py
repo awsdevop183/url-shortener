5e5e5fc844c2203211173525df0a88eee497f838
shreyashjoshi <43450174+shreyashjoshi@users.noreply.github.com>
2026-06-10 22:53:32 +0530

    Update redirect_url.py

--- redirect_url.py
+++ redirect_url.py
@@ -2,7 +2,7 @@ import json
 import boto3
 
 dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('url-shortener')
+table = dynamodb.Table('url-shortner')
 
 
 def lambda_handler(event, context):
